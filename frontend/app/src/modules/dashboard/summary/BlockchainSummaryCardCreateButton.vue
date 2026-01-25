acfb9e99594ccd539f124915fa09004d5e004a57
Alex Miller <alexlmiller@users.noreply.github.com>
2026-01-25 09:08:21 -0700

    Add Stacks to dashboard blockchain address dropdown

--- frontend/app/src/modules/dashboard/summary/BlockchainSummaryCardCreateButton.vue
+++ frontend/app/src/modules/dashboard/summary/BlockchainSummaryCardCreateButton.vue
@@ -18,6 +18,10 @@ const blockchainCategories = [{
   label: t('dashboard.blockchain_balances.categories.solana'),
   path: '/accounts/solana',
 }, {
+  icon: 'lu-layers',
+  label: t('dashboard.blockchain_balances.categories.stacks'),
+  path: '/accounts/stacks',
+}, {
   icon: 'lu-substrate-accounts',
   label: t('dashboard.blockchain_balances.categories.substrate'),
   path: '/accounts/substrate',
